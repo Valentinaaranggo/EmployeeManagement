5b74357d490045565c2dc90281dd7954f2e54d61
mduruji <mczduruji@gmail.com>
2024-04-17 15:00:15 -0400

    First Commit

---
+++ Address.java
@@ -0,0 +1,46 @@
+package Employee_Functions;
+public class Address {
+    String _street;
+    String _suiteNumber;
+    String _zip;
+    String _city;
+    String _state;
+    String _country;
+    
+    public String get_street() {
+        return _street;
+    }
+    public void set_street(String street) {
+        this._street = street;
+    }
+    public String get_suiteNumber() {
+        return _suiteNumber;
+    }
+    public void set_suiteNumber(String suiteNumber) {
+        this._suiteNumber = suiteNumber;
+    }
+    public String get_zip() {
+        return _zip;
+    }
+    public void set_zip(String zip) {
+        this._zip = zip;
+    }
+    public String get_city() {
+        return _city;
+    }
+    public void set_city(String city) {
+        this._city = city;
+    }
+    public String get_state() {
+        return _state;
+    }
+    public void set_state(String state) {
+        this._state = state;
+    }
+    public String get_country() {
+        return _country;
+    }
+    public void set_country(String country) {
+        this._country = country;
+    }
+}
---
+++ UpdateEmployeeSalary.java
@@ -0,0 +1,4 @@
+package Employee_Functions;
+public class UpdateEmployeeSalary {
+    
+}
