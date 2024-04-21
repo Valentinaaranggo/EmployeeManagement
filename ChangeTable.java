9fe5ced0c11aede48e9d7969ab7c973d5a085b35
mduruji <mczduruji@gmail.com>
2024-04-20 22:10:14 -0400

    Making print statements shorter.

--- ChangeTable.java
+++ ChangeTable.java
@@ -1,5 +1,6 @@
 package EmployeeManagement;
 import java.sql.*;
+import static java.lang.System.out;
 public class ChangeTable {
     
     public void addColumn (String columnName, String dataType, Connection myConn1) {
@@ -8,6 +9,7 @@ public class ChangeTable {
             Statement myStmt = myConn1.createStatement();
             myStmt.executeUpdate(sqlcommand1);
             myConn1.close();
+            out.println("\nColumn created successfully.");
         }catch (Exception e) {
             System.out.println("ERROR " + e.getLocalizedMessage());
         } finally {
--- SearchEmployee.java
+++ SearchEmployee.java
@@ -1,5 +1,6 @@
 package EmployeeManagement;
 import java.sql.*;
+import static java.lang.System.out;
 public class SearchEmployee {
     
 }
